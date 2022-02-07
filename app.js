33003ac3c849ca60f9afda25ff3e1d894361edab
Tamir Ganbat <tamir.g@premiumbm.mn>
2022-02-07 19:26:14 +0800

    Event listeneruudiig uusgesen

---
+++ app.js
@@ -0,0 +1,57 @@
+// //IFEE
+
+// // private encapsulation
+// var uiController = (function(){
+//     // pricate data
+//     var x=100;
+//     //private function
+//     function add(y){
+//         return x+y;
+//     }
+
+//     return{
+//         publicAdd:function(a){
+
+//         }
+//     }
+// })();
+
+// // private function
+// // private data
+// // private encapsulation
+
+// var financeController=(function(){
+//     console.log("D");
+// })();
+
+// var appController=(function(uiController, fnController){
+//     uiController.add(50);
+  
+// })(uiController,financeController);
+
+// //IFEE
+
+// delgetstei ajillah controller
+var uiController = (function(){
+
+})();
+
+// sanhuutei ajillah controller
+
+var financeController=(function(){
+
+})();
+
+//programmin holbogch controller
+
+var appController=(function(uiController, fnController){
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        
+    });
+    document.addEventListener('keypress', function(event){
+
+    });
+    
+  
+})(uiController,financeController);
+
