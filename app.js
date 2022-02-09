d7a202e13599d06b6b19a8cd46ba3a101ada9566
Tamir Ganbat <tamir.g@premiumbm.mn>
2022-02-09 18:45:21 +0800

    sanhuugiin model oruulah

--- app.js
+++ app.js
@@ -63,6 +63,32 @@ var uiController = (function(){
 
 var financeController=(function(){
 
+    var Income=function(id,description, value){
+        this.id=id;
+        this.description=description;
+        this.value=value;
+    };
+    
+    var Expense=function(id, description, value){
+        this.id=id;
+        this.description=description;
+        this.value=value;
+
+    };
+
+    var data={
+        allItems:{
+            inc:[],
+            exp:[]
+        },
+
+        totals:{
+            inc=0,
+            exp:0
+        }
+        
+    }
+
 })();
 
 //programmin holbogch controller
