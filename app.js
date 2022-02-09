a8e31d87493b5d8f2f995bd6d7b8bb447a750686
Tamir Ganbat <tamir.g@premiumbm.mn>
2022-02-09 18:20:00 +0800

    commit hiiv

--- app.js
+++ app.js
@@ -31,8 +31,31 @@
 
 // //IFEE
 
+/*** Endees ehelj baina */
+
 // delgetstei ajillah controller
 var uiController = (function(){
+    var DOMstrings={
+        inputType:".add__type",
+        inputDescription:".add__description",
+        inputValue:".add__value",
+        addBtn:".add__btn"
+    }
+
+
+    return {
+        getInput:function(){
+            return{
+              type:document.querySelector(DOMstrings.inputType).value, 
+              description: document.querySelector(DOMstrings.inputDescription).value,
+              value:document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDOMstrings:function(){
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -45,13 +68,42 @@ var financeController=(function(){
 //programmin holbogch controller
 
 var appController=(function(uiController, fnController){
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        
-    });
-    document.addEventListener('keypress', function(event){
 
-    });
+    
+    var ctrlAddItem=function(){
+        //1. oguulah ugugdliig ddelgetsees awna
+        
+        console.log(uiController.getInput());
+        //2. olj awsan ogogdluudee web deeree tohiroh hesegt ni gargah
+        //3. Olj awsan ogogdluudee delgetsin tohiroh hesegt ni gargana
+        //4. tosviig tootsoolno
+        //5. etssiin uldegdel, tootsoot delgetsend gargana
+    };
+    
+    var setupEventListeners=function(){
+
+        var DOM=uiController.getDOMstrings();
+
+        document.querySelector(DOM.addBtn).addEventListener('click', function(){
+            ctrlAddItem();
+        });
+        document.addEventListener('keypress', function(event){
+            if(event.keyCode===13 || event.which===13){
+                ctrlAddItem();
+            }
+    
+        });
+
+    };
+    return{
+        init:function(){
+            console.log("App has started...")
+        }
+    };
+    
     
   
 })(uiController,financeController);
 
+
+appController.init();
